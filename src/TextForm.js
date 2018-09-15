7610d5b63c4f17cf86317009094b09df385b1c57
Hong Yi Chen <hongyigma@gmail.com>
2018-09-15 18:35:57 -0400

    Updated merged files.

--- src/TextForm.js
+++ src/TextForm.js
@@ -4,19 +4,21 @@ class TextForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'How do you feel...'
+      value: 'Let your emotions do the talking...'
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
+  // SUBMIT BUTTON FUNCTION
   handleSubmit(event) {
-    alert('Your response has been saved: ' + this.state.value);
+    alert('An essay was submitted: ' + this.state.value);
     event.preventDefault();
   }
 
@@ -25,11 +27,17 @@ class TextForm extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           <textarea value={this.state.value} onChange={this.handleChange} />
+
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Enter Journal"
+        />
       </form>
     );
   }
+
+//           textarea.style.height = ""; /* Reset the height*/
+        //  textarea.style.height = Math.min(text.scrollHeight, heightLimit) + "px";
+
 }
 
 export default TextForm;
