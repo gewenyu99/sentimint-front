bc7f4dcf2fba8342384c11e40a3c697cf23bc624
wen <wenyu1999@hotmail.com>
2018-09-15 22:04:24 -0400

    added submissions and response for NLP

--- src/TextForm.js
+++ src/TextForm.js
@@ -1,35 +1,49 @@
 import React from 'react';
 
 class TextForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'How do you feel...'
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "What's on your mind?",
+            sentiment: ''
+        };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
 
-  handleSubmit(event) {
-    alert('Your response has been saved: ' + this.state.value);
-    event.preventDefault();
-  }
+    handleSubmit(event) {
+        event.preventDefault();
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", 'https://us-central1-sentimint-e1de7.cloudfunctions.net/helloWorld', true);
+        // xhttp.open("POST", 'http://localhost:5000/sentimint-e1de7/us-central1/helloWorld', true);
+        xhttp.send(this.state.value);
+        var that = this;
+        xhttp.onreadystatechange = function (e) {
+            console.log(xhttp.responseText);
+            e.preventDefault();
+            that.setState({sentiment: xhttp.responseText});
+            document.getElementById("sentiment_text").innerHTML = that.state.sentiment;
+        };
+    }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <textarea value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <textarea value={this.state.value} onChange={this.handleChange}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+                <p id="sentiment_text"> </p>
+            </div>
+        );
+    }
 }
 
 export default TextForm;
