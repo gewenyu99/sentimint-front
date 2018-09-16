240f74a5d727143d2d4bfe25cefda31bf62f618d
Faris Fizal <farisfizal@gmail.com>
2018-09-16 00:32:47 -0400

    lmao

--- src/App.js
+++ src/App.js
@@ -7,21 +7,37 @@ import InfoModal from './InfoModal';
 import './App.css';
 
 class App extends Component {
+  status() {
+    if (this.props.listening == true) {
+      this.props.stopListening()
+    } else {
+      this.props.startListening()
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header row">
           <img src={logo} className="App-logo col-md-4" alt="logo" />
-          <InfoModal class="modal-icon col-md-4 offset-md-4" />
+          <InfoModal class="col-md-4 offset-md-4" />
         </header>
-        <TextForm />
-        <SpeechRecognition>
-          abc
-        </SpeechRecognition>
+        <TextForm value={this.props.transcript} />
+        <button className="speechButton" onClick={()=>{this.status()}}>{this.props.listening ? "Recording" : "Not Recording"}</button>
+        <button className="speechButton" onClick={this.props.resetTranscript}>Reset</button>
         <img src={arrow} className="floating-arrow floating" />
       </div>
     );
   }
 }
 
-export default App;
+App.defaultProps = {
+  transcript: '',
+  resetTranscript: '',
+  browserSupportsSpeechRecognition: true
+};
+
+const options = {
+  autoStart: false
+}
+
+export default SpeechRecognition(options)(App);
--- src/TextForm.js
+++ src/TextForm.js
@@ -1,35 +1,42 @@
 import React from 'react';
+import './TextForm.css';
+import TextareaAutosize from 'react-autosize-textarea';
 
 class TextForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'How do you feel...'
-    };
+ constructor(props) {
+   super(props);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+   this.state = {
+     value: ''
+   };
+   this.handleChange = this.handleChange.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
+ }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+ handleChange(event) {
+   this.setState({value: event.target.value});
+ }
 
-  handleSubmit(event) {
-    alert('Your response has been saved: ' + this.state.value);
-    event.preventDefault();
-  }
+ // SUBMIT BUTTON FUNCTION
+ handleSubmit(event) {
+   alert('A journal was submitted: ' + this.state.value);
+   event.preventDefault();
+ }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <textarea value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+ componentWillReceiveProps(nextProps) {
+   this.setState({value: nextProps.value})
+ }
+
+ render() {
+   return (
+     <form onSubmit={this.handleSubmit}>
+       <label>
+         <TextareaAutosize onChange={this.handleChange} value={this.state.value} placeholder = "Let your emotions do the talking..."/>
+       </label>
+       <input type="submit" value="Enter Journal"/>
+     </form>
+   );
+ }
 }
 
 export default TextForm;
